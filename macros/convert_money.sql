1a77f6495267d22f8aba08319109b72e7026bbb9
Alex Dinsmoor <alex.dinsmoor@fishtownanalytics.com>
2022-11-22 05:21:24 +0000

    revert logic (remove models, sources, and template macro files to be used in session 2)

--- macros/convert_money.sql
+++
@@ -1,3 +0,0 @@
-{% macro money(col) -%}
-::decimal(16,3)
-{%- endmacro %}
--- models/demo_examples/c_custom_macro_pivot_by_ship_mode.sql
+++
@@ -1,9 +0,0 @@
-with fct_order_items as (
-    select * from {{ ref('fct_order_items') }}
-)
-
-select
-    ship_mode, 
-    {{ custom_pivot( 'fct_order_items', 'ship_mode' ) }}
-from {{ ref('fct_order_items') }}
-group by 1
